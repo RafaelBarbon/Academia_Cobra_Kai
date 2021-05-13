c9eee6df487665c51f8ed160f22463ba8b0c7c2e
Henrique <hss@outlook.com.br>
2021-05-13 17:36:28 -0300

    Atualiação da estrutura do banco de dados
    Correção de erros na estrutura e atualização da mesma.
    
    Inserção das informações básicas para que o programa funcione

--- src/banco_de_dados.sql
+++ src/banco_de_dados.sql
@@ -4,72 +4,75 @@ default collate utf8_general_ci;
 
 use academia;
 
-create table if not exists aulas{
-	codigo varchar(5),
+create table if not exists banco(
+	cpf_aluno varchar(15) not null,
+	pago boolean,
+	primary key (cpf_aluno)
+)default charset = utf8;
+
+create table if not exists aulas(
+	codigo varchar(5) not null,
 	horario varchar(20),
 	faixa int,
 	armas boolean,
 	primary key (codigo)
-}default charset = utf8;
+)default charset = utf8;
 
-create table if not exists alunos{
-	codigo varchar(5),
+create table if not exists alunos(
+	codigo varchar(5) not null,
 	nome varchar(50),
 	cpf varchar(15),
 	email varchar(50),
 	telefone varchar(16),
 	atraso int,
-	mes-pago boolean,
-	valor decimal(4,2),
+	mes_pago boolean,
+	valor decimal(5,2),
 	nascimento date, /*yyyy-mm-dd*/
 	faixa int,
 	auxiliar boolean,
 	senha varchar(20),
 	primary key (codigo),
-	primary key (cpf)
-}default charset = utf8;
+	foreign key (cpf) references banco(cpf_aluno)
+)default charset = utf8;
 
-create table if not exists professores{
-	codigo varchar(5),
+create table if not exists professores(
+	codigo varchar(5) not null,
 	nome varchar(50),
-	cpf varchar(15),
+	cpf varchar(15) unique,
 	email varchar(50),
 	telefone varchar(16),
 	senha varchar(20),
-	nascimento date /*yyyy-mm-dd*/
-	primary key (codigo),
-	primary key (cpf)
-}default charset = utf8;
+	nascimento date, /*yyyy-mm-dd*/
+	primary key (codigo)
+)default charset = utf8;
 
-create table if not exists matriculas{
-	id varchar(5),
-	id-aluno varchar(5),
-	id-aula varchar(5),
+create table if not exists matriculas(
+	id varchar(5) not null,
+	id_aluno varchar(5),
+	id_aula varchar(5),
 	primary key (id),
-	foreign key (id-aluno) references alunos(codigo),
-	foreign key (id-aula) references aulas(codigo)
-}default charset = utf8;
+	foreign key (id_aluno) references alunos(codigo),
+	foreign key (id_aula) references aulas(codigo)
+)default charset = utf8;
 
-create table if not exists acessos{
-	id varchar(5),
-	id-professor varchar(5),
+create table if not exists acessos(
+	id varchar(5) not null,
+	id_professor varchar(5) not null,
 	horario datetime, /*yyyy-MM-dd hh-mm-ss*/
 	primary key (id),
-	foreign key (id-professor) references professores(codigo)
-}default charset = utf8;
+	foreign key (id_professor) references professores(codigo)
+)default charset = utf8;
 
-create table if not exists banco{
-	cpf varchar(15),
-	pago boolean,
-	primary key (cpf)
-}default charset = utf8;
+create table if not exists info(
+	id varchar(2) not null,
+	v1aula decimal(5,2) not null,
+	v2aula decimal(5,2) not null,
+	v3aula decimal(5,2) not null,
+	v4aula decimal(5,2) not null,
+	senha_mestre varchar(20) not null,
+	senha_email varchar(20) not null,
+	email varchar(50) not null,
+	primary key(id)
+)default charset = utf8;
 
-create table if not exists info{
-	v1aula decimal(4,2),
-	v2aula decimal(4,2),
-	v3aula decimal(4,2),
-	v4aula decimal(4,2),
-	senha-mestre varchar(20),
-	senha-email varchar(20),
-	email varchar(20)
-}default charset = utf8;
+insert ignore into info values ('1',100.00,170.00,200.00,250.00,'admin','Hd@4149!','dev.hours88@gmail.com');
